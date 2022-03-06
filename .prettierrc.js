d9aec56bcd2541d0014cfbf0051270a9014f70c3
Seob <yeongseob.dev@gmail.com>
2022-03-06 20:57:26 +0900

    CRA-typescript + eslint + prettier

---
+++ .prettierrc.js
@@ -0,0 +1,15 @@
+// json파일은 주석을 달 수 없으므로 파일 확장자를 js로 했습니다.
+// https://prettier.io/docs/en/options.html
+module.exports = {
+  printWidth: 80, // default 80
+  tabWidth: 2, // default 2
+  useTabs: false, // default false
+  semi: true, // default true
+  singleQuote: true, // default false
+  quoteProps: 'as-needed', // default as-needed
+  jsxSingleQuote: true, // default false
+  trailingComma: 'es5', // default es5
+  bracketSpacing: true, //default true
+  bracketSameLine: false, //default false
+  arrowParens: 'always', // default always
+};
